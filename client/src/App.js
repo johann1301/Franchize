40183d0cb225803bcc45569982b304e6d7edaa2d
Johannes von Kessel <johannes.v.kessel@web.de>
2022-01-14 15:09:59 +0100

    show events Frontend

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import Navbar from './components/Navbar';
+import EventCard from './components/EventCard';
 
 
 
@@ -16,6 +17,7 @@ function App() {
       <Routes>
 
         <Route path='/' element={<Home />} />
+        <Route path='/api/events' element={<EventCard />} />
         
       </Routes>
 
